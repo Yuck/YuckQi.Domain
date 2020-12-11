748df2122de81acf7d0f6c5b22493bf7662d9e1d
Kevin J Lambert <kevin.lambert@poconosystems.com>
2020-12-11 18:36:43 -0500

    Added DomainValidationException

---
+++ src/YuckQi.Domain.Validation/DomainValidationException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace YuckQi.Domain.Validation
+{
+    public class DomainValidationException : ApplicationException
+    {
+        #region Constants
+
+        private const string DefaultErrorMessage = "Something went wrong.";
+
+        #endregion
+
+
+        #region Properties
+
+        public Result Result { get; }
+
+        #endregion
+
+
+        #region Constructors
+
+        public DomainValidationException(Result result) : base(GetErrorMessage(result))
+        {
+            Result = result;
+        }
+
+        #endregion
+
+
+        #region Supporting Methods
+
+        private static string GetErrorMessage(Result result)
+        {
+            if (result == null)
+                return DefaultErrorMessage;
+
+            var count = result.Detail.Count(t => t.Type == ResultType.Error);
+            var noun = count == 1 ? "error" : "errors";
+            var message = $"Domain model has {count} {noun} after validation.";
+
+            return message;
+        }
+
+        #endregion
+    }
+}
