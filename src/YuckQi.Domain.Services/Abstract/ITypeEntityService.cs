50ac85bcd671dd1fbe5e0ab12684d3ae03f0ec92
Kevin J Lambert <kevin.lambert@poconosystems.com>
2020-12-12 12:08:22 -0500

    Added a domain services project

---
+++ src/YuckQi.Domain.Services/Abstract/ITypeEntityService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using YuckQi.Domain.Application.Queries.Results;
+using YuckQi.Domain.Entities.Types;
+using YuckQi.Domain.Services.Models;
+using YuckQi.Domain.Validation;
+
+namespace YuckQi.Domain.Services.Abstract
+{
+    public interface ITypeEntityService<T, TKey> where T : TypeEntityBase<TKey> where TKey : struct
+    {
+        Task<Result<Page<T, TKey>>> SearchAsync(TypeSearchCriteria criteria = null);
+    }
+}
---
+++ src/YuckQi.Domain.Services/Models/TypeSearchCriteria.cs
@@ -0,0 +1,12 @@
+﻿using YuckQi.Domain.Application.Abstract;
+
+namespace YuckQi.Domain.Services.Models
+{
+    public class TypeSearchCriteria : IPage
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
