254c2dff406e7d8d4f03473975ec89e3b836afc3
Kevin J Lambert <kevin.lambert@poconosystems.com>
2021-01-05 18:15:29 -0500

    Modified to return an IPage instead of Page

--- src/YuckQi.Domain.Services/Abstract/ITypeEntityService.cs
+++ src/YuckQi.Domain.Services/Abstract/ITypeEntityService.cs
@@ -3,16 +3,16 @@ using System.Threading.Tasks;
 using YuckQi.Domain.Entities.Types.Abstract;
 using YuckQi.Domain.Services.Models;
 using YuckQi.Domain.Validation;
-using YuckQi.Domain.ValueObjects;
+using YuckQi.Domain.ValueObjects.Abstract;
 
 namespace YuckQi.Domain.Services.Abstract
 {
-    public interface ITypeEntityService<T, in TKey> where T : ITypeEntity<TKey> where TKey : struct
+    public interface ITypeEntityService<TTypeEntity, in TKey> where TTypeEntity : ITypeEntity<TKey> where TKey : struct
     {
-        Task<Result<T>> CreateAsync(T entity);
-        Task<Result<T>> GetAsync(Guid identifier);
-        Task<Result<T>> GetAsync(TKey key);
-        Task<Result<T>> ModifyAsync(T entity);
-        Task<Result<Page<T>>> SearchAsync(TypeSearchCriteria criteria = null);
+        Task<Result<TTypeEntity>> CreateAsync(TTypeEntity entity);
+        Task<Result<TTypeEntity>> GetAsync(Guid identifier);
+        Task<Result<TTypeEntity>> GetAsync(TKey key);
+        Task<Result<TTypeEntity>> ModifyAsync(TTypeEntity entity);
+        Task<Result<IPage<TTypeEntity>>> SearchAsync(TypeSearchCriteria criteria = null);
     }
 }
