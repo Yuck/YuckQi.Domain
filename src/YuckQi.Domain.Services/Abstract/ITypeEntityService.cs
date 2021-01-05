33f40bd6330319b4de333fd3ff09d02dfead64af
Kevin J Lambert <kevin.lambert@poconosystems.com>
2021-01-05 07:11:56 -0500

    Modified ITypeEntityService to include get by identifier

--- src/YuckQi.Domain.Services/Abstract/ITypeEntityService.cs
+++ src/YuckQi.Domain.Services/Abstract/ITypeEntityService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using YuckQi.Domain.Entities.Types.Abstract;
 using YuckQi.Domain.Services.Models;
 using YuckQi.Domain.Validation;
@@ -9,6 +10,7 @@ namespace YuckQi.Domain.Services.Abstract
     public interface ITypeEntityService<T, in TKey> where T : ITypeEntity<TKey> where TKey : struct
     {
         Task<Result<T>> CreateAsync(T entity);
+        Task<Result<T>> GetAsync(Guid identifier);
         Task<Result<T>> GetAsync(TKey key);
         Task<Result<T>> ModifyAsync(T entity);
         Task<Result<Page<T>>> SearchAsync(TypeSearchCriteria criteria = null);
--- src/YuckQi.Domain.Services/Models/TypeSearchCriteria.cs
+++ src/YuckQi.Domain.Services/Models/TypeSearchCriteria.cs
@@ -4,7 +4,6 @@ namespace YuckQi.Domain.Services.Models
 {
     public class TypeSearchCriteria : IPage
     {
-        public string Code { get; set; }
         public string Name { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
