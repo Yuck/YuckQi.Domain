911b815ae050090bf7791281783c5f37b2a7f0d0
Kevin J Lambert <kevin.lambert@poconosystems.com>
2022-06-07 08:20:57 -0400

    Added support for ValidateAsync

--- src/YuckQi.Domain.Validation/Extensions/AbstractValidatorExtensions.cs
+++ src/YuckQi.Domain.Validation/Extensions/AbstractValidatorExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using FluentValidation;
 using FluentValidation.Results;
 
@@ -10,14 +11,29 @@ public static class AbstractValidatorExtensions
 {
     public static Result<T> GetResult<T>(this AbstractValidator<T> validator, T item, String failedValidationMessageId)
     {
-        var result = validator.Validate(item);
-        if (result == null)
-            throw new ArgumentNullException(nameof(result));
+        var validationResult = validator.Validate(item);
+        var result = BuildResult(validationResult, item, failedValidationMessageId);
 
-        if (result.IsValid)
+        return result;
+    }
+
+    public static async Task<Result<T>> GetResultAsync<T>(this AbstractValidator<T> validator, T item, String failedValidationMessageId)
+    {
+        var validationResult = await validator.ValidateAsync(item);
+        var result = BuildResult(validationResult, item, failedValidationMessageId);
+
+        return result;
+    }
+
+    private static Result<T> BuildResult<T>(ValidationResult validationResult, T item, String failedValidationMessageId)
+    {
+        if (validationResult == null)
+            throw new ArgumentNullException(nameof(validationResult));
+
+        if (validationResult.IsValid)
             return new Result<T>(item);
 
-        return new Result<T>(default, GetResultDetail(result, failedValidationMessageId));
+        return new Result<T>(default, GetResultDetail(validationResult, failedValidationMessageId));
     }
 
     private static IReadOnlyCollection<ResultDetail> GetResultDetail(ValidationResult result, String failedValidationMessageId)
