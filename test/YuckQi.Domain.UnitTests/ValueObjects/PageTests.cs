979dac0c61a0e32f8861af279cc60ff6b9aedf8c
Kevin J Lambert <kevin.lambert@poconosystems.com>
2023-03-16 17:20:27 -0400

    Added unit tests for YuckQi.Domain

---
+++ test/YuckQi.Domain.UnitTests/ValueObjects/PageTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using YuckQi.Domain.ValueObjects;
+
+namespace YuckQi.Domain.UnitTests.ValueObjects;
+
+public class PageTests
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Page_Properties_HaveExpectedValues()
+    {
+        var page = new Page(1, 10);
+
+        Assert.That(page.PageNumber, Is.EqualTo(1));
+        Assert.That(page.PageSize, Is.EqualTo(10));
+    }
+
+    [Test]
+    public void PageOfString_Properties_HaveExpectedValues()
+    {
+        var items = new HashSet<String> { "abc", "def", "ghi" };
+        var page = new Page<String>(items, items.Count, 1, 10);
+
+        Assert.That(page.Items, Contains.Item("abc"));
+        Assert.That(page.Items, Contains.Item("def"));
+        Assert.That(page.Items, Contains.Item("ghi"));
+        Assert.That(page.PageNumber, Is.EqualTo(1));
+        Assert.That(page.PageSize, Is.EqualTo(10));
+        Assert.That(page.TotalCount, Is.EqualTo(3));
+    }
+}
