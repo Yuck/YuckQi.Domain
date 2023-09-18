07fc1cfa6bb05bcfe95b50d49fa39cc2cd16a3b7
Kevin J Lambert <kevin.lambert@poconosystems.com>
2023-09-18 06:34:26 -0400

    object extensions for changed properties

---
+++ src/YuckQi.Domain/Extensions/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace YuckQi.Domain.Extensions;
+
+public static class ObjectExtensions
+{
+    private static readonly ConcurrentDictionary<Type, IEnumerable<PropertyInfo>> PropertiesByType = new ();
+
+    public static Dictionary<PropertyInfo, (Object?, Object?)> GetDifferencesByProperty<T>(this T a, T b)
+    {
+        var properties = PropertiesByType.GetOrAdd(typeof(T), type => type.GetProperties());
+        var values = properties.Select(t => new KeyValuePair<PropertyInfo, (Object?, Object?)>(t, (t.GetValue(a), t.GetValue(b))));
+        var differences = values.Where(t => ! Equals(t.Value.Item1, t.Value.Item2)).ToDictionary(t => t.Key, t => t.Value);
+
+        return differences;
+    }
+}
---
+++ test/YuckQi.Domain.UnitTests/Extensions/ObjectExtensionTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using YuckQi.Domain.Extensions;
+
+namespace YuckQi.Domain.UnitTests.Extensions;
+
+public class ObjectExtensionTests
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Objects_WithDifferentStringValues_HaveOneDifference()
+    {
+        var a = new { a = 123, b = "test" };
+        var b = new { a = 123, b = "hello" };
+        var differences = a.GetDifferencesByProperty(b);
+
+        Assert.That(differences, Has.Exactly(1).Items);
+        Assert.That(differences.Single().Key.Name, Is.EqualTo(nameof(a.b)));
+        Assert.That(differences.Single().Value.Item1, Is.EqualTo("test"));
+        Assert.That(differences.Single().Value.Item2, Is.EqualTo("hello"));
+    }
+}
